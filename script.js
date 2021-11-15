24ace882840db0350b262a8606962c4ff2f65b86
names-james-home <contact.jameslloyd@gmail.com>
2021-11-15 17:55:42 -0500

    Color picker added, working on rainbow

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const grayscale = document.querySelector('#grayscaleMode');
 const rainbow = document.querySelector('#rainbowMode');
 const erase = document.querySelector('#eraserMode');
 const clear = document.querySelector('#clearGrid');
+const colorSelector = document.querySelector('#colorSelector');
 
 const slider = document.querySelector('#sizeSlider');
 const sliderText = document.querySelector('#sliderText');
+
+let style = 'color';
+color.onclick = () => style = 'color';
+grayscale.onclick = () => style = 'grayscale';
+rainbow.onclick = () => style = 'rainbow';
+erase.onclick = () => style = 'erase';
+clear.onclick = () => makeGrid(slider.value);
+
 sliderText.innerHTML = `${slider.value} x ${slider.value}`;
 makeGrid(slider.value);
 slider.oninput = function() {
@@ -15,16 +24,9 @@ slider.oninput = function() {
     makeGrid(this.value);
 }
 
-
-let style = 'color';
-color.onclick = () => style = 'color';
-grayscale.onclick = () => style = 'grayscale';
-rainbow.onclick = () => style = 'rainbow';
-erase.onclick = () => erase();
-clear.onclick = () => resetGrid();
-
 function makeGrid(size) {
-    grid.setAttribute('style', `grid-template-columns: repeat(${size}, 20px [col-start]);grid-template-rows: repeat(${size}, 20px [row-start]);`);
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${size}, [col-start] minmax(100px, auto-fit) [col-end]);grid-template-rows: repeat(${size}, [row-start] minmax(100px, auto-fit) [row-end]);`);
     for (c = 1; c <= size; c++) { //populates each row
         for (r = 1; r <= size; r++) { //populates a row
         const space = document.createElement('div');
@@ -37,17 +39,38 @@ function makeGrid(size) {
     boxes.forEach(space => space.addEventListener('mouseover', highlightSpace));
 }
 
-
 function highlightSpace(event){
-    event.target.classList.add(`${style}`);
+    switch (style) {
+        case 'color':
+            event.target.style.backgroundColor = `${colorSelector.value}`;
+            break;
+        case 'grayscale':
+            event.target.style.backgroundColor = 'blue';
+            break;
+        case 'rainbow':
+            event.target.style.backgroundColor = cycleColors;
+        case 'erase':
+            event.target.style.backgroundColor = 'white';
+    }
 }
+
 function resetGrid() {
     const boxes = document.querySelectorAll('.space');
-    boxes.forEach(space => space.classList.remove('color', 'grayscale', 'rainbow'));
+    boxes.forEach(space => space.setAttribute('style', 'background-color: none;'));
  }
 
+ function removeAllChildNodes(parent) {
+     while (parent.firstChild) {
+         parent.removeChild(parent.firstChild);
+     }
+ }
 
-
+ function cycleColors() { //needs work
+    for (let i = 0; i = 100; i++) {
+        let colorHSL = "hsl(" + (360 * i / 24) + ",80%,50%)";
+        return colorHSL;
+      }
+}
 
 
 
