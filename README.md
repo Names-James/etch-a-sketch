add8a02e6b2d720b93506e6df0ba8693bb5b4627
names-james-home <contact.jameslloyd@gmail.com>
2021-11-17 19:33:54 -0500

    all basic functionality!

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # etch-a-sketch
+I felt really happy with this challenge. The directions stated that it should be difficult, and I was pretty proud when my first instinct to create the grid using nested loops worked. I assigned the class 'space' to each box as well as a starting background color. Both of these gave me something to target with my code immediately after the grids creation.
+
+My biggest challenges were learning the basic css-grid syntax, saving RGB values to an array, and using the Math function to generate the types of values I wanted. Google solved these quickly, however I should practice working with these more.
+
+Design choices:
+--(script 48-54) I choise to tackle grayscale first of the optional features. I already had the idea to save the rgb values into an array, edit the values, and join them back together. I wanted to check if the cell was a shade of gray, darken the shade, or turn it a shade of gray that was not white.
+--(script 14-19) I used the variable 'style' to make the decision of which effect to apply to a space that is moused-over.
+
+
+What I learned:
+--(script 78-87) Shorthand for applying array methods at the same time as declaring the array.
+--(script 86) Using .map to return a new array with post-fuction/method array values.
+--(script 58-60) Removing the background color with the erase function would break the space, and not allow any following effects to be applied.
+--(script 35, 59) Assigning the value of 'white' to the background would break the functions that required rgb values to manipulate.
+--(script 29) In order to create a new grid, I had to destroy the old one or the spaces would remain.
--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const rainbow = document.querySelector('#rainbowMode');
 const erase = document.querySelector('#eraserMode');
 const clear = document.querySelector('#clearGrid');
 const colorSelector = document.querySelector('#colorSelector');
+colorSelector.style.backgroundColor = `${colorSelector.value}`;
+colorSelector.oninput = () => colorSelector.style.backgroundColor = `${colorSelector.value}`;
 
 const slider = document.querySelector('#sizeSlider');
 const sliderText = document.querySelector('#sliderText');
@@ -19,19 +21,18 @@ clear.onclick = () => makeGrid(slider.value);
 sliderText.innerHTML = `${slider.value} x ${slider.value}`;
 makeGrid(slider.value);
 slider.oninput = function() {
-    resetGrid();
     sliderText.innerHTML = `${this.value} x ${this.value}`;
     makeGrid(this.value);
 }
 
 function makeGrid(size) {
-    removeAllChildNodes(grid);
+    removeAllChildNodes(grid); //needed to destroy the previous grid
     grid.setAttribute('style', `grid-template-columns: repeat(${size}, [col-start] minmax(100px, auto-fit) [col-end]);grid-template-rows: repeat(${size}, [row-start] minmax(100px, auto-fit) [row-end]);`);
-    for (c = 1; c <= size; c++) { //populates each row
-        for (r = 1; r <= size; r++) { //populates a row
+    for (c = 1; c <= size; c++) {
+        for (r = 1; r <= size; r++) {
         const space = document.createElement('div');
         space.classList.add('space');
-        space.setAttribute('style', `display: grid; grid-area: ${r} / ${c};`);
+        space.setAttribute('style', `background-color: rgb(255,255,255); display: grid; grid-area: ${r} / ${c};`);
         grid.appendChild(space);
         }
     }
@@ -45,19 +46,20 @@ function highlightSpace(event){
             event.target.style.backgroundColor = `${colorSelector.value}`;
             break;
         case 'grayscale':
-            event.target.style.backgroundColor = 'blue';
+            const bgColor = rgbToArray(event.target.style.backgroundColor);
+            if (bgColor.every((val) => val === bgColor[0])) {
+                event.target.style.backgroundColor = darkenRgb(bgColor);
+                break;
+            } else event.target.style.backgroundColor = 'rgb(220,220,220)';
             break;
         case 'rainbow':
-            event.target.style.backgroundColor = cycleColors;
+            event.target.style.backgroundColor = randomRgb();
+            break;
         case 'erase':
-            event.target.style.backgroundColor = 'white';
+            event.target.style.backgroundColor = 'rgb(255,255,255)';
+            break;
     }
 }
-
-function resetGrid() {
-    const boxes = document.querySelectorAll('.space');
-    boxes.forEach(space => space.setAttribute('style', 'background-color: none;'));
- }
 
  function removeAllChildNodes(parent) {
      while (parent.firstChild) {
@@ -65,13 +67,43 @@ function resetGrid() {
      }
  }
 
- function cycleColors() { //needs work
-    for (let i = 0; i = 100; i++) {
-        let colorHSL = "hsl(" + (360 * i / 24) + ",80%,50%)";
-        return colorHSL;
-      }
+ function randomRgb() {
+        let num = Math.round(0xffffff * Math.random());
+        let r = num >> 16;
+        let g = num >> 8 & 255;
+        let b = num & 255;
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
 
+function rgbToArray(rgbString) {
+    // First convert to an array of integers by removing the whitespace,
+    //taking the 3rd char to the 2nd last then splitting by ','
+    const rgbIntArray = rgbString
+      .replace(/ /g, "")
+      .slice(4, -1)
+      .split(",")
+      .map((e) => parseInt(e));
 
+    return rgbIntArray;
+}
 
+function darkenRgb(rgbArray) {
+    for (i=0; i <= 2; i++) {
+        rgbArray[i] = +rgbArray[i] - 25;
+    }
+    return 'rgb(' + rgbArray.join(', ') + ')'
+}
 
+//button mouse events
+color.addEventListener('mouseenter', ()=> color.classList.add('hover'));
+color.addEventListener('mouseleave', ()=> color.classList.remove('hover'));
+grayscale.addEventListener('mouseenter', ()=> grayscale.classList.add('hover'));
+grayscale.addEventListener('mouseleave', ()=> grayscale.classList.remove('hover'));
+rainbow.addEventListener('mouseenter', ()=> rainbow.classList.add('hover'));
+rainbow.addEventListener('mouseleave', ()=> rainbow.classList.remove('hover'));
+erase.addEventListener('mouseenter', ()=> erase.classList.add('hover'));
+erase.addEventListener('mouseleave', ()=> erase.classList.remove('hover'));
+clear.addEventListener('mouseenter', ()=> clear.classList.add('hover'));
+clear.addEventListener('mouseleave', ()=> clear.classList.remove('hover'));
+colorSelector.addEventListener('mouseenter', ()=> colorSelector.classList.add('hover'));
+colorSelector.addEventListener('mouseleave', ()=> colorSelector.classList.remove('hover'));
